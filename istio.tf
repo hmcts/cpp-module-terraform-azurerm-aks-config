0bde1bd0c98e77314dec732435808e58f6327b30
Shashikant Bangera <shashi@Scrumconnects-MacBook-Pro.local>
2021-08-18 11:17:27 +0100

    EI-80 changes added
    Change-Id: Icf3ebd07c2fa0503c842043e521d7de907545711

--- istio.tf
+++ istio.tf
@@ -17,7 +17,7 @@ resource "kubernetes_namespace" "istio_namespace" {
   }
 }
 
-# Apply IstioOperator manifest to the operator
+# Apply IstioOperator manifest to the operator 
 resource "kubectl_manifest" "istio_operator_manifest" {
   yaml_body = templatefile("${path.module}/manifests/istio.yaml", {
     istio_node_selector                        = var.istio_node_selector_label
