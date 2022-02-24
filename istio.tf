1256ef5f2a7e8614b8fb87fc6e140b7a6be50df1
saalim.khan <saalim.khan@hmcts.net>
2022-02-24 11:36:23 +0000

    added peerauthentication for mtls
    Change-Id: I65ae6f31b5e6924fab80543098d82cee83b6d527

--- istio.tf
+++ istio.tf
@@ -58,3 +58,29 @@ resource "time_sleep" "wait_for_istio_crds" {
 
   create_duration = "30s"
 }
+
+# Istio gateway menifest 
+
+data "kubectl_file_documents" "istio_gateway_manifests" {
+  content = templatefile("${path.module}/manifests/istio/istio_gateway.yaml", {
+    ingress-gateway-secret = var.istio_gateway_cert_secret_name
+    ingressdomain          = var.ingressdomain
+    aks_cluster_name       = var.aks_cluster_name
+  })
+}
+
+resource "kubectl_manifest" "istio_gateway_manifest" {
+  count     = length(data.kubectl_file_documents.istio_gateway_manifests.documents)
+  yaml_body = element(data.kubectl_file_documents.istio_gateway_manifests.documents, count.index)
+  depends_on = [
+    kubectl_manifest.cert-manager-install,
+    kubectl_manifest.cert_issuer_install,
+    time_sleep.wait_for_istio_crds
+  ]
+}
+
+# PeerAuthentication menifest - Enable mtls 
+
+resource "kubectl_manifest" "enable_mtls" {
+  yaml_body = file("${path.module}/manifests/istio/istio_peerauthentication.yaml")
+}
