48dfec48726cc96659b108fba33b0965672621e3
Abhivan Chekuri <abhivan.chekuri@hmcts.net>
2022-09-13 11:11:42 +0100

    Add rolebinding for jenkins namespace -02
    Change-Id: Ia080e1b1507381b274d096fcd16a8e6ee2042f95

--- jenkins.tf
+++ jenkins.tf
@@ -11,6 +11,7 @@ resource "kubernetes_namespace" "jenkins_namespace" {
 
 resource "kubectl_manifest" "jenkins_deploy_rolebinding" {
   yaml_body          = <<YAML
+apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
 metadata:
   name: jenkins-deploy
