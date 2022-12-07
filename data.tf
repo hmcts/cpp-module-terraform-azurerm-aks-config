231c4feedfca622b792007d145f76d527ec8d172
Abhivan Chekuri <abhivan.chekuri@hmcts.net>
2022-12-07 15:27:29 +0000

    Output Getway IP's using kubernetes provider

--- data.tf
+++ data.tf
@@ -1,5 +1,34 @@
-data "azurerm_lb" "ingress_lb" {
-  name                = var.istio_ingress_load_balancer_name
+resource "time_sleep" "wait_for_loadbalancer" {
+  depends_on = [
+    helm_release.istio_ingress_mgmt_install,
+    helm_release.istio_ingress_apps_install,
+    kubectl_manifest.install_istio_ingress_gateway_manifests
+  ]
+  create_duration = "120s"
+}
+
+data "kubernetes_service" "mgmt_gateway_svc" {
+  metadata {
+    name      = "istio-ingressgateway-mgmt"
+    namespace = "istio-ingress"
+  }
+  depends_on = [
+    time_sleep.wait_for_loadbalancer
+  ]
+}
+
+data "kubernetes_service" "app_gateway_svc" {
+  metadata {
+    name      = "istio-ingressgateway-apps"
+    namespace = "istio-ingress"
+  }
+  depends_on = [
+    time_sleep.wait_for_loadbalancer
+  ]
+}
+
+data "azurerm_private_link_service" "ingress_apps" {
+  name                = "PLS-${upper(var.aks_cluster_name)}-INGRESS-APPS"
   resource_group_name = var.istio_ingress_load_balancer_resource_group
   depends_on = [
     helm_release.istio_ingress_mgmt_install,
@@ -8,8 +37,8 @@ data "azurerm_lb" "ingress_lb" {
   ]
 }
 
-data "azurerm_private_link_service" "ingress_apps" {
-  name                = "PLS-${upper(var.aks_cluster_name)}-INGRESS-APPS"
+data "azurerm_private_link_service" "ingress_mgmt" {
+  name                = "PLS-${upper(var.aks_cluster_name)}-INGRESS-MGMT"
   resource_group_name = var.istio_ingress_load_balancer_resource_group
   depends_on = [
     helm_release.istio_ingress_mgmt_install,
--- outputs.tf
+++ outputs.tf
@@ -10,14 +10,18 @@ output "ingress_apps_domain_name" {
   value = split("*", var.istio_ingress_apps_domains[0])[1]
 }
 
-output "ingress_lb_private_ip_address" {
-  value = data.azurerm_lb.ingress_lb.private_ip_address
+output "ingress_lb_private_ip_address_apps" {
+  value = data.kubernetes_service.app_gateway_svc.status.0.load_balancer.0.ingress.0.ip
 }
 
-output "ingress_lb_private_ip_addresses" {
-  value = data.azurerm_lb.ingress_lb.private_ip_addresses
+output "ingress_lb_private_ip_address_mgmt" {
+  value = data.kubernetes_service.mgmt_gateway_svc.status.0.load_balancer.0.ingress.0.ip
 }
 
 output "private_link_service_ingress_apps_id" {
   value = data.azurerm_private_link_service.ingress_apps.id
+}
+
+output "private_link_service_ingress_mgmt_id" {
+  value = data.azurerm_private_link_service.ingress_mgmt.id
 }
