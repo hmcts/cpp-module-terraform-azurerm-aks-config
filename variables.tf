914ef40770e9c8d9284d639a2b8758e1a73b0173
Olu Ayodele <olu.ayodele@hmcts.net>
2023-01-05 01:29:24 +0000

    removed pgadmin sample password

--- variables.tf
+++ variables.tf
@@ -552,6 +552,7 @@ variable "pgadmin_admin_password" {
   type        = string
   description = "enable pgadmin interface within cluster"
   }
+  
 variable "istio_ingress_load_balancer_name" {
   type        = string
   default     = "kubernetes-internal"
