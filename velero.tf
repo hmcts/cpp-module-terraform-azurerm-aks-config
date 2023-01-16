dc82102ca8e0a7d8445973ac6c29723313bbcbb4
varunvijayagopal <varun.vijayagopal@hmcts.net>
2023-01-16 12:13:14 +0000

    create storage account

--- velero.tf
+++ velero.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "storage_account_velero" {
   count                     = var.velero_config.enable ? 1 : 0
-  name                      = "SA-${var.aks_cluster_name}"
+  name                      = lower("SA-${var.aks_cluster_name}")
   resource_group_name       = var.aks_resource_group_name
   location                  = var.aks_cluster_location
   account_tier              = var.velero_config.account_tier
