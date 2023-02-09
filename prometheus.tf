6d38bbb2d9560824af7b4318c8f4be4c36684ba6
saalim.khan <saalim.khan@hmcts.net>
2023-02-09 11:21:14 +0000

    Updated the backup label dEI-985

--- prometheus.tf
+++ prometheus.tf
@@ -4,6 +4,7 @@ resource "kubernetes_namespace" "prometheus_namespace" {
     labels = {
       "app.kubernetes.io/managed-by" = "Terraform"
       "filebeat_enable"              = "enabled"
+      "backup_enabled"               = "true"
     }
   }
   depends_on = [time_sleep.wait_for_aks_api_dns_propagation]
