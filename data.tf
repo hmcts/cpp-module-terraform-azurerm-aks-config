d90140eed5f12bf1000127afc65f9ffeff313414
gunnertwin <daniel@alvestechnology.io>
2023-06-14 09:43:05 +0100

    feat: add web istio gateway

--- data.tf
+++ data.tf
@@ -27,6 +27,16 @@ data "kubernetes_service" "app_gateway_svc" {
   ]
 }
 
+# data "kubernetes_service" "web_gateway_svc" {
+#   metadata {
+#     name      = "istio-ingressgateway-web"
+#     namespace = "istio-ingress"
+#   }
+#   depends_on = [
+#     time_sleep.wait_for_loadbalancer
+#   ]
+# }
+
 data "azurerm_private_link_service" "ingress_apps" {
   name                = "PLS-${upper(var.aks_cluster_name)}-INGRESS-APPS"
   resource_group_name = var.istio_ingress_load_balancer_resource_group
@@ -43,6 +53,16 @@ data "azurerm_private_link_service" "ingress_mgmt" {
   depends_on = [
     helm_release.istio_ingress_mgmt_install,
     helm_release.istio_ingress_apps_install,
+    kubectl_manifest.install_istio_ingress_gateway_manifests
+  ]
+}
+
+data "azurerm_private_link_service" "ingress_web" {
+  name                = "PLS-${upper(var.aks_cluster_name)}-INGRESS-WEB"
+  resource_group_name = var.istio_ingress_load_balancer_resource_group
+  depends_on = [
+    helm_release.istio_ingress_mgmt_install,
+    helm_release.istio_ingress_web_install,
     kubectl_manifest.install_istio_ingress_gateway_manifests
   ]
 }
