367bd36c50c86b0136bf89e22340a6b3f67e7399
Jyothi Balla <jyothi.balla@hmcts.net>
2023-07-12 17:51:35 +0100

    revert

--- main.tf
+++ main.tf
@@ -28,12 +28,6 @@ data "kubectl_file_documents" "network_policy_manifests" {
 resource "kubectl_manifest" "install_mgmt_networkpolicies" {
   count              = length(split("\n---\n", file("${path.module}/manifests/common/networkpolicy.yaml")))
   yaml_body          = element(data.kubectl_file_documents.network_policy_manifests.documents, count.index)
-
-  dynamic override_namespace {
-    for_each = toset(var.system_namespaces)
-    content {
-      override_namespace = "$each.key"
-    }
-  }
+  override_namespace = [for ns in var.system_namespaces : ns]
   depends_on         = [kubernetes_namespace.prometheus_namespace, kubernetes_namespace.sonarqube_namespace, kubernetes_namespace.kiali_namespace, kubernetes_namespace.pgadmin_namespace]
 }
