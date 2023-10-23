24ba5068b5be41b9a231e81bd17f940d72b76d36
Jyothi Balla <jyothi.balla@hmcts.net>
2023-10-23 17:16:36 +0100

    key reso

--- variables.tf
+++ variables.tf
@@ -818,7 +818,7 @@ variable "aso_resources" {
 variable "resource_types" {
   description = "List of Azure resource types for which to retrieve predefined roles"
   type        = list(string)
-  default     = ["Microsoft.Storage/storageAccounts", "Microsoft.KeyVault/vaults"]
+  default     = ["Key Vault Administrator", "Key Vault Contributor", "Storage Account Contributor", "Storage Blob Data Owner"]
 }
 
 variable "enable_azureinfo" {
