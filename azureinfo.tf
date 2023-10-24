b548d3c14da605accc54b2310eb40e563ffb0009
Jyothi Balla <jyothi.balla@hmcts.net>
2023-10-24 15:19:13 +0100

    manifest

--- azureinfo.tf
+++ azureinfo.tf
@@ -9,9 +9,17 @@ resource "kubernetes_namespace" "azure_info_namespace" {
   depends_on = [time_sleep.wait_for_aks_api_dns_propagation]
 }
 
+resource "azurerm_resource_group" "aks" {
+  name     = "RG-MI-${var.aks_cluster_name}"
+  location = var.aks_cluster_location
+  tags     = var.tags
+  lifecycle {
+    ignore_changes = [tags["created_by"], tags["created_time"]]
+  }
+}
 
 data "azurerm_role_definition" "predefined_roles" {
-  for_each = { for role in var.resource_types : role => role }
+  for_each = { for role in var.resource_types : replace(role, " ", "_") => role }
 
   name = each.value
 }
@@ -22,15 +30,31 @@ locals {
   }
 }
 
-
-resource "kubectl_manifest" "store_azure_info" {
-  count = var.enable_azureinfo ? 1 : 0
-  yaml_body = templatefile("${path.module}/manifests/common/azureinfo.yaml", {
+data "kubectl_file_documents" "azure_info_manifests" {
+  content = templatefile("${path.module}/manifests/common/azureinfo.yaml", {
     oidc_issuer_url  = data.azurerm_kubernetes_cluster.aks_cluster.oidc_issuer_url
     subscription_id  = data.azurerm_client_config.current.subscription_id
     tenant_id        = data.azurerm_client_config.current.tenant_id
-    role_definitions = join("\n", [for role_name, role_id in local.role_definitions : "${role_name}: ${role_id}"])
+    role_definitions = join("\n    ", [for role_name, role_id in local.role_definitions : "${role_name}: ${role_id}"])
     namespace        = kubernetes_namespace.azure_info_namespace[0].metadata.0.name
+    mi_resource_group = azurerm_resource_group.aks.name
   })
-  depends_on = [time_sleep.wait_for_aks_api_dns_propagation]
 }
+
+resource "kubectl_manifest" "store_azure_info" {
+  for_each   = { for i, value in data.kubectl_file_documents.azure_info_manifests.documents : i => value }
+  yaml_body = each.value
+  depends_on = [time_sleep.wait_for_aks_api_dns_propagation, kubernetes_namespace.azure_info_namespace]
+}
+
+#resource "kubectl_manifest" "store_azure_info" {
+#  yaml_body = templatefile("${path.module}/manifests/common/azureinfo.yaml", {
+#        oidc_issuer_url  = data.azurerm_kubernetes_cluster.aks_cluster.oidc_issuer_url
+#        subscription_id  = data.azurerm_client_config.current.subscription_id
+#        tenant_id        = data.azurerm_client_config.current.tenant_id
+#        role_definitions = join("\n    ", [for role_name, role_id in local.role_definitions : "${role_name}: ${role_id}"])
+#        namespace        = kubernetes_namespace.azure_info_namespace[0].metadata.0.name
+#        mi_resource_group = azurerm_resource_group.aks.name
+#  })
+#  depends_on = [time_sleep.wait_for_aks_api_dns_propagation]
+#}
