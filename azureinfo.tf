25f2de863adc1b60a3b0c738f9fa726968222d41
Jyothi Balla <jyothi.balla@hmcts.net>
2023-10-24 15:50:10 +0100

    fix precommit

--- azureinfo.tf
+++ azureinfo.tf
@@ -32,11 +32,11 @@ locals {
 
 data "kubectl_file_documents" "azure_info_manifests" {
   content = templatefile("${path.module}/manifests/common/azureinfo.yaml", {
-    oidc_issuer_url  = data.azurerm_kubernetes_cluster.aks_cluster.oidc_issuer_url
-    subscription_id  = data.azurerm_client_config.current.subscription_id
-    tenant_id        = data.azurerm_client_config.current.tenant_id
-    role_definitions = join("\n    ", [for role_name, role_id in local.role_definitions : "${role_name}: ${role_id}"])
-    namespace        = kubernetes_namespace.azure_info_namespace[0].metadata.0.name
+    oidc_issuer_url   = data.azurerm_kubernetes_cluster.aks_cluster.oidc_issuer_url
+    subscription_id   = data.azurerm_client_config.current.subscription_id
+    tenant_id         = data.azurerm_client_config.current.tenant_id
+    role_definitions  = join("\n    ", [for role_name, role_id in local.role_definitions : "${role_name}: ${role_id}"])
+    namespace         = kubernetes_namespace.azure_info_namespace[0].metadata.0.name
     mi_resource_group = azurerm_resource_group.aks.name
   })
 }
@@ -44,17 +44,5 @@ data "kubectl_file_documents" "azure_info_manifests" {
 resource "kubectl_manifest" "store_azure_info" {
   count      = length(split("\n---\n", file("${path.module}/manifests/common/azureinfo.yaml")))
   yaml_body  = element(data.kubectl_file_documents.azure_info_manifests.documents, count.index)
-  depends_on = [time_sleep.wait_for_aks_api_dns_propagation, kubernetes_namespace.azure_info_namespace, data.kubectl_file_documents.azure_info_manifests]
+  depends_on = [time_sleep.wait_for_aks_api_dns_propagation, kubernetes_namespace.azure_info_namespace]
 }
-
-#resource "kubectl_manifest" "store_azure_info" {
-#  yaml_body = templatefile("${path.module}/manifests/common/azureinfo.yaml", {
-#        oidc_issuer_url  = data.azurerm_kubernetes_cluster.aks_cluster.oidc_issuer_url
-#        subscription_id  = data.azurerm_client_config.current.subscription_id
-#        tenant_id        = data.azurerm_client_config.current.tenant_id
-#        role_definitions = join("\n    ", [for role_name, role_id in local.role_definitions : "${role_name}: ${role_id}"])
-#        namespace        = kubernetes_namespace.azure_info_namespace[0].metadata.0.name
-#        mi_resource_group = azurerm_resource_group.aks.name
-#  })
-#  depends_on = [time_sleep.wait_for_aks_api_dns_propagation]
-#}
