73cad9b6bb9216759caa3c8f56baa2d8467a4f18
tolga-hmcts <tolga.ingenc@hmcts.net>
2024-04-16 16:42:01 +0100

    Parameterise helm binary [EI-1790]

--- download_charts.tf
+++ download_charts.tf
@@ -1,9 +1,20 @@
 # Need to improve auth to ACR. The helm provider v2 should be able to do it but cannot get it to working. Need to revisit.
 # https://stackoverflow.com/questions/59565463/deploying-helm-charts-via-terraform-helm-provider-and-azure-devops-while-fetchin
+
+locals {
+  charts_info = [for chart in var.charts : {
+    path       = chart.path
+    version    = chart.version
+    dir        = "install/${element(split("/", chart.path), 1)}"
+    chart_name = "${element(split("/", chart.path), 1)}"
+  }]
+}
+
 resource "null_resource" "download_charts" {
   triggers = {
     always_run = "${timestamp()}"
   }
+
 
   provisioner "local-exec" {
     command = <<-EOT
@@ -11,15 +22,14 @@ resource "null_resource" "download_charts" {
       export HELM_EXPERIMENTAL_OCI=1
       HELM_BINARY=$${HELM_BINARY:-helm}
       $$HELM_BINARY registry login ${var.acr_name}.azurecr.io --username ${var.acr_user_name} --password ${var.acr_user_password}
-      %{for chart in var.charts}
-      if [ "$$HELM_BINARY" = "helm-3.14.2" ]; then
-        if [ -d "./install/$$(basename ${chart.path})" ]; then
-          rm -rf "./install/$$(basename ${chart.path})"
+      %{for chart in local.charts_info~}
+        if [ -d "${chart.dir}" ]; then
+          rm -rf "${chart.dir}"
         fi
         mkdir -p ./install
         $$HELM_BINARY pull oci://${var.acr_name}.azurecr.io/${chart.path} --version ${chart.version} --destination ./install
-        tar zxvf ./install/$$(basename ${chart.path})-${chart.version}.tgz -C ./install
-        rm -f ./install/$$(basename ${chart.path})-${chart.version}.tgz
+        tar zxvf ${chart.dir}/${chart.version}.tgz -C ${chart.chart_name}
+        rm -f ${chart.dir}-${chart.version}.tgz
       else
         $$HELM_BINARY chart remove ${var.acr_name}.azurecr.io/${chart.path}:${chart.version}
         $$HELM_BINARY chart pull ${var.acr_name}.azurecr.io/${chart.path}:${chart.version}
