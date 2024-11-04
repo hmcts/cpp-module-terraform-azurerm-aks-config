7b4b0b7d1cc9a36051742f07c076236143ff0b35
Joshua Glass <joshua.glass@justice.gov.uk>
2024-11-04 15:38:08 +0000

    4.x updates

--- velero.tf
+++ velero.tf
@@ -1,13 +1,13 @@
 resource "azurerm_storage_account" "storage_account_velero" {
-  count                     = var.velero_config.enable ? 1 : 0
-  name                      = replace(lower("SA-${var.aks_cluster_name}"), "-", "")
-  resource_group_name       = var.aks_resource_group_name
-  location                  = var.aks_cluster_location
-  account_tier              = var.velero_config.account_tier
-  is_hns_enabled            = true
-  account_replication_type  = var.velero_config.account_replication_type
-  tags                      = var.tags
-  enable_https_traffic_only = true
+  count                      = var.velero_config.enable ? 1 : 0
+  name                       = replace(lower("SA-${var.aks_cluster_name}"), "-", "")
+  resource_group_name        = var.aks_resource_group_name
+  location                   = var.aks_cluster_location
+  account_tier               = var.velero_config.account_tier
+  is_hns_enabled             = true
+  account_replication_type   = var.velero_config.account_replication_type
+  tags                       = var.tags
+  https_traffic_only_enabled = true
 }
 
 resource "azurerm_storage_container" "blob" {
