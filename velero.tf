94a5b0917146addccc078d7e739b86f25914baf3
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2025-01-31 10:13:29 +0000

    update azurerrm version to 3.117.0

--- velero.tf
+++ velero.tf
@@ -1,12 +1,13 @@
 resource "azurerm_storage_account" "storage_account_velero" {
-  count                    = var.velero_config.enable ? 1 : 0
-  name                     = replace(lower("SA-${var.aks_cluster_name}"), "-", "")
-  resource_group_name      = var.aks_resource_group_name
-  location                 = var.aks_cluster_location
-  account_tier             = var.velero_config.account_tier
-  is_hns_enabled           = true
-  account_replication_type = var.velero_config.account_replication_type
-  tags                     = var.tags
+  count                      = var.velero_config.enable ? 1 : 0
+  name                       = replace(lower("SA-${var.aks_cluster_name}"), "-", "")
+  resource_group_name        = var.aks_resource_group_name
+  location                   = var.aks_cluster_location
+  account_tier               = var.velero_config.account_tier
+  is_hns_enabled             = true
+  account_replication_type   = var.velero_config.account_replication_type
+  tags                       = var.tags
+  https_traffic_only_enabled = true
 }
 
 resource "azurerm_storage_container" "blob" {
